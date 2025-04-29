1367b9eb544cf0bf071fc71b765058a3ffb62ced
menegrete <martin.negrete@gmail.com>
2025-04-29 06:16:10 -0300

    added logging

--- src/generate.ts
+++ src/generate.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { getConfig } from './config';
 import { getCommits, getIssuesPath } from './git';
 import { Changelog, generateChangelog } from './changelog';
+import { info } from '@actions/core';
 
 type Generate = (params: {
   from: string;
@@ -13,6 +14,10 @@ type Generate = (params: {
 const generate: Generate = async ({ from, to, configFile, repoUrl }) => {
   const config = getConfig(
     configFile || path.join(__dirname, 'defaultConfig.json')
+  );
+
+  info(
+    'Generating changelog...' + JSON.stringify({ from, to, repoUrl, ...config })
   );
   const issuesUrl = config.issuesUrl || (await getIssuesPath({ repoUrl }));
   const commits = await getCommits({ from, to });
